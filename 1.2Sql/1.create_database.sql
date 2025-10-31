86e7752cabac538c8f450ecdff60b121d8ea5f7d
Thichaona Sithole <thichaona.sithole@sigmaconnected.com>
2025-10-31 08:07:02 +0200

    Fixing sql codes

--- 1.2Sql/1.create_database.sql
+++ 1.2Sql/1.create_database.sql
@@ -1,6 +1,8 @@
+
+--Create staging Databases
+
 CREATE DATABASE computer_staging;
 
---------staging
+--------Create Dataware House Database
 CREATE DATABASE Computers_dataware_house;
 
------------
